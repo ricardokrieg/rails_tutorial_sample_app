89a1e6b714ef99438fe209632d1536f0bef70871
Ricardo Krieg <ricardo.krieg@gmail.com>
2013-02-18 02:42:46 -0300

    ...

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,6 +9,11 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "has the right title" do
+      get 'home'
+      response.should have_selector('title', :content => '| Home')
+    end
   end
 
   describe "GET 'contact'" do
@@ -16,12 +21,22 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+
+    it "has the right title" do
+      get 'contact'
+      response.should have_selector('title', :content => '| Contact')
+    end
   end
 
   describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
+    end
+
+    it "has the right title" do
+      get 'about'
+      response.should have_selector('title', :content => '| About')
     end
   end
 
